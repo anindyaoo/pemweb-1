dd058d8fb3044a781d24a2aa669de8921eda2a13
pemweb1 <aniendyao@gmail.com>
2024-11-01 19:40:36 +0700

    add

---
+++ Tugas minggu6/contoh2/script.js
@@ -0,0 +1,6 @@
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Tambahkan fungsi JavaScript lainnya jika diperlukan
