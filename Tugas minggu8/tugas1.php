9b7f8f3b4d1f7400d1dc1aa5f4559b653a9499f6
pemweb1 <aniendyao@gmail.com>
2024-11-17 23:19:00 +0700

    add

--- Tugas minggu8/tugas1.php
+++ Tugas minggu8/tugas1.php
@@ -1,3 +1,11 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF dalam PHP</title>
+</head>
 <body>
     <h1>Pengecek Nilai</h1>
     <form method="post" action="">
